51183b3e130681508eb5bd09e7c302bb1e09797a
RashmiBasnet <rashmibasnet2019@gmail.com>
2025-12-18 13:38:52 +0545

    Theme

---
+++ lib/screen/bottom_screen/venue_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VenueScreen extends StatelessWidget {
+  const VenueScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Center(
+        child: Text("This is Venue Screen"),
+      ),
+    );
+  }
+}
---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:venue_connect/app.dart';
+
+ThemeData getApplicationTheme() {
+    return ThemeData(
+      primaryColor: kPrimaryDark,
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: "Poppins Regular",
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kAccentGold,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 14),
+        ),
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        selectedItemColor: Color(0xFFAE8E54),
+        selectedLabelStyle: TextStyle(
+          fontFamily: "Poppins SemiBold",
+          fontSize: 15
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontSize: 15
+        )
+      )
+    );
+  }
