5e0baf6498a2b40ffab31c826ea2e3d44038a6c3
RashmiBasnet <rashmibasnet2019@gmail.com>
2025-12-18 13:37:29 +0545

    Theme Data and Bottom Navigation Bar Created

---
+++ lib/screen/bottom_screen_layout.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:venue_connect/screen/bottom_screen/activity_screen.dart';
+import 'package:venue_connect/screen/bottom_screen/home_screen.dart';
+import 'package:venue_connect/screen/bottom_screen/package_screen.dart';
+import 'package:venue_connect/screen/bottom_screen/profile_screen.dart';
+import 'package:venue_connect/screen/bottom_screen/venue_screen.dart';
+
+class BottomScreenLayout extends StatefulWidget {
+  const BottomScreenLayout({super.key});
+
+  @override
+  State<BottomScreenLayout> createState() => _BottomScreenLayoutState();
+}
+
+class _BottomScreenLayoutState extends State<BottomScreenLayout> {
+  int _selectedIndex = 0;
+  
+  List<Widget> listBottomScreen = [
+    const HomeScreen(),
+    const VenueScreen(),
+    const PackageScreen(),
+    const ActivityScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: listBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+
+        items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.pie_chart_outline), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home_work_outlined), label: 'Venue'),
+        BottomNavigationBarItem(icon: Icon(Icons.credit_card_outlined), label: 'Package'),
+        BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: 'Activity'),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Profile'),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: (index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      
+      )
+    );
+  }
+} 
