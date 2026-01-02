e1fc55b2725402b5fb0d1698ae020fc038715bbc
RashmiBasnet <rashmibasnet2019@gmail.com>
2026-01-02 14:54:08 +0545

    Initiated Hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:venue_connect/app/app.dart';
+import 'package:venue_connect/core/services/hive_service.dart';
 
-void main() {
-  runApp(App());
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HiveService().init();
+
+  runApp(ProviderScope(child: App()));
 }
